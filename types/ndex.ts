ec597b2328b648f994a125ec5e3c521c1d5c2cbe
LiponRoy <liponbdbd@gmail.com>
2023-11-07 13:22:24 +0600

    Getting current user and logOut system

--- types/ndex.ts
+++ types/ndex.ts
@@ -1,4 +1,15 @@
 import { MouseEventHandler } from 'react';
+import {User } from "@prisma/client";
+
+
+export type SafeUser = Omit<
+  User,
+  "createdAt" | "updatedAt" | "emailVerified"
+> & {
+  createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
+};
 
 export interface InavLink {
 	id: number;
