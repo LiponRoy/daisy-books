0639220ba2dab77b1db10309a2f2bb1371e1890f
LiponRoy <liponbdbd@gmail.com>
2023-11-14 13:35:02 +0600

    created reusable search component

---
+++ components/search/Search.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { RiMenuSearchLine } from "react-icons/ri";
+
+interface Isearch {
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const Search = ({ value, onChange }: Isearch) => {
+  return (
+    <div className=" flex justify-end items-center mb-4  ">
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={value}
+        onChange={onChange}
+        className=" w-full outline-none py-2 my-4 pl-2 border-2 border-light_green rounded-md"
+      />
+      <div className=" text-light_green">
+      <RiMenuSearchLine size={42} />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
